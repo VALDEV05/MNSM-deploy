1fb34e549414671a1b84e11280b18b813ca8d5c5
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-01-02 16:26:23 +0100

    test topbarheader

--- wp-content/themes/aurum/header.php
+++ wp-content/themes/aurum/header.php
@@ -1,35 +1,51 @@
 <?php
+
 /**
  *	Aurum WordPress Theme
  *
  *	Laborator.co
  *	www.laborator.co
  */
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Direct access not allowed.
 }
 
 ?>
 <!DOCTYPE html>
+<!-- =============================================================================================================== -->
+<!-- =============================================================================================================== -->
+<!-- =============================================================================================================== -->
+<!-- =============================================================================================================== -->
+<!-- @author Valerio Corda -->
+<!-- @email info@valeriocorda.it -->
+<!-- @website https://valeriocorda.it -->
+<!-- If you can dream it, I can code it.  -->
+<!-- =============================================================================================================== -->
+<!-- =============================================================================================================== -->
+<!-- =============================================================================================================== -->
+<!-- =============================================================================================================== -->
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
-	<body <?php body_class(); ?>>
-		
-		<?php
-			/**
-			 * Hooks before header
-			 */
-			do_action( 'aurum_body_start' );
-				
-			/**
-			 * Site header
-			 */
-			if ( ! defined( 'LAB_HEADERLESS' ) ) {
-				get_template_part( 'tpls/header' );
-			}
-		?>
+
+
+<body <?php body_class(); ?>>
+
+	<?php
+	/**
+	 * Hooks before header
+	 */
+	do_action('aurum_body_start');
+
+	/**
+	 * Site header
+	 */
+	if (!defined('LAB_HEADERLESS')) {
+		get_template_part('tpls/header');
+	}
+	?>
--- wp-content/themes/aurum/tpls/header-top-bar.php
+++ wp-content/themes/aurum/tpls/header-top-bar.php
@@ -1,97 +1,16 @@
 <?php
-/**
- *	Aurum WordPress Theme
- *
- *	Laborator.co
- *	www.laborator.co
- */
-
-if ( ! get_data( 'header_top_links' ) ) {
-	return;
-} 
-
-$header_top_style       = get_data( 'header_top_style' );
-$header_top_links_left  = get_data( 'header_top_links_left' );
-$header_top_links_right = get_data( 'header_top_links_right' );
-
-$header_top_left_text   = get_data( 'header_top_left_text' );
-$header_top_right_text  = get_data( 'header_top_right_text' );
-
-// Left column
-$left_column = $header_top_links_left || $header_top_left_text;
-
-// Right column
-$right_column = $header_top_links_right || $header_top_right_text;
-
-// Classes
-$classes = array( 'top-menu' );
-
-// Top menu skin
-if ( in_array( $header_top_style, array( 'gray', 'light' ) ) ) {
-	$classes[] = "top-menu--{$header_top_style}";
-} else {
-	$classes[] = 'top-menu--dark';
-}
-
-// Columns
-if ( $left_column && $right_column ) {
-	$classes[] = 'top-menu--columns-2';
-}
-
 ?>
-<div <?php aurum_class_attr( $classes ); ?>>
-	
-	<div class="container">
-		
-		<div class="row">
-		
-			<?php if ( $left_column ) : ?>
-			<div class="col">
-				
-				<?php
-					
-					/**
-					 * Show top menu text
-					 */
-					if ( ! empty( $header_top_left_text ) ) {
-						aurum_show_top_menu_widget( $header_top_left_text, true );
-					}
-					
-					/**
-					 * Show top menu widget
-					 */
-					if ( 'hide' !== $header_top_links_left ) {
-						aurum_show_top_menu_widget( $header_top_links_left );
-					}
-				?>
-				
-			</div>
-			<?php endif; ?>
-			
-			<?php if ( $right_column ) : ?>
-			<div class="col right">
-				
-				<?php
-					/**
-					 * Show top menu widget
-					 */
-					if ( 'hide' !== $header_top_right_text ) {
-						aurum_show_top_menu_widget( $header_top_links_right );
-					}
-					
-					/**
-					 * Show top menu text
-					 */
-					if ( ! empty( $header_top_right_text ) ) {
-						aurum_show_top_menu_widget( $header_top_right_text, true );
-					}
-				?>
-				
-			</div>
-			<?php endif; ?>
-			
-		</div>
-		
-	</div>
-	
+<div class="site-header_top-bar">
+    <div class="container">
+        <div class="row">
+            <div class="text-centered">
+                Oggi 31 Agosto 2023 siamo aperti 9.30 - 19.30
+            </div>
+            <div class="social-menu_widget">
+                <?php @include('../widget_custom_social-network.php');?>
+            </div>
+        </div>
+    </div>
 </div>
+</div>
+<!-- /.site-header_top-bar -->
