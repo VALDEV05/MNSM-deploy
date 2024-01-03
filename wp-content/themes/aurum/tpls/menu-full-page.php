da9dcec8ae8c92aa0f9291aa451c99e5d2107d23
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-01-03 12:40:05 +0100

    add custom logo background menu

--- wp-content/themes/aurum/tpls/menu-full-page.php
+++ wp-content/themes/aurum/tpls/menu-full-page.php
@@ -1,4 +1,10 @@
 <div class="menu-full-page-custom-mnsm">
-    
+    <div class="container_custom-menu-full-page">
+        <?php $strumento_musicale = get_field('strumento_musicale_menu_full_page', 'option');
+        if (!empty($strumento_musicale)) : ?>
+            <img src="<?php echo esc_url($strumento_musicale['url']); ?>" alt="<?php echo esc_attr($strumento_musicale['alt']); ?>" />
+        <?php endif; ?>
+    </div>
+    <!-- /.container_custom-menu-full-page -->
 </div>
 <!-- /.menu-full-page-custom-mnsm -->
