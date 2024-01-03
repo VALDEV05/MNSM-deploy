3983f4528e96ec1a506fce02df0942a31ac664ff
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-01-03 12:06:42 +0100

    complete layout header

--- wp-content/themes/aurum/tpls/header-logo-secondary.php
+++ wp-content/themes/aurum/tpls/header-logo-secondary.php
@@ -1,13 +1,16 @@
 <div class="container-header-logo-secondary">
     <div class="container-header-logo-secondary container-header-logo-secondary_first-logo">
-        <?php $first_logo = get_field('header-first-logo');
+        <?php $first_logo = get_field('header-first-logo', 'option');
         if (!empty($first_logo)) : ?>
             <img src="<?php echo esc_url($first_logo['url']); ?>" alt="<?php echo esc_attr($first_logo['alt']); ?>" />
         <?php endif; ?>
     </div>
     <!-- /.container-header-logo-secondary container-header-logo-secondary_first-logo -->
     <div class="container-header-logo-secondary container-header-logo-secondary_second-logo">
-
+        <?php $second_logo = get_field('header-second-logo', 'option');
+        if (!empty($second_logo)) : ?>
+            <img src="<?php echo esc_url($second_logo['url']); ?>" alt="<?php echo esc_attr($second_logo['alt']); ?>" />
+        <?php endif; ?>
     </div>
     <!-- /.container-header-logo-secondary container-header-logo-secondary_second-logo -->
 </div>
