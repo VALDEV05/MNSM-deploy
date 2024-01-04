c20f38f99166aa8840e6f2f1f9666e31511db29b
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-01-04 12:44:45 +0100

    start code footer

--- wp-content/themes/aurum/tpls/footer.php
+++ wp-content/themes/aurum/tpls/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *	Aurum WordPress Theme
  *
@@ -8,69 +9,32 @@
 
 $footer_style               = get_data('footer_style');
 $footer_widgets             = get_data('footer_widgets');
-$footer_widgets_collapsed   = get_data( 'footer_collapse_mobile' );
+$footer_widgets_collapsed   = get_data('footer_collapse_mobile');
 $footer_text                = get_data('footer_text');
 $footer_text_right          = get_data('footer_text_right');
 $footer_menu                = ltrim(get_data('footer_menu'), '_');
 
 ?>
-<footer class="site-footer<?php echo $footer_style ? " {$footer_style}" : ''; echo ! $footer_widgets ? ' no-footer-widgets' : ''; ?>">
+<div class="site-footer-element-decoration">
+        <svg id="Livello_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2400 374.08">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #161513;
+                        stroke-width: 0px;
+                    }
+                </style>
+            </defs>
+            <g id="Livello_1-2">
+                <path class="cls-1" d="M2400,374.08c-73.59-130.02-223.61-269.14-547.56-186.69-92.17,21.04-176.95,27.86-247.13-24.64,0,0,29.97,59.51,14.04,98.18-15.75,38.47-108.68,103.29-306.88,91.16-198.01-12.32-271.79-141.96-245.42-268,0,0-69.99,38.66-124.42,21.04C888.38,87.51,662.3-14.08,488.76,1.65,352.77,13.97,102.42,40.12,0,373.32" />
+            </g>
+        </svg>
+    </div>
+<footer class="site-footer">
+    
+    <div class="container">
 
-	<div class="container">
 
-		<?php if ( $footer_widgets ) : ?>
-		
-		<?php if ( $footer_widgets_collapsed ) : ?>
-		<div class="row visible-xs">
-			<div class="col-lg-12">
-				<a href="#" class="expand-footer"></a>
-			</div>
-		</div>
-		<?php endif; ?>
-
-		<div class="row <?php echo $footer_widgets_collapsed ? 'hidden-xs' : ''; ?> footer-widgets">
-
-			<?php dynamic_sidebar('footer_sidebar'); ?>
-
-		</div>
-		<?php endif; ?>
-
-		<div class="footer-bottom">
-
-			<div class="row">
-				<?php if($footer_text || $footer_menu): ?>
-				<div class="col-md-<?php echo $footer_text_right ? 6 : 12; ?>">
-					<?php echo do_shortcode($footer_text); ?>
-					<?php echo $footer_text && $footer_menu ? '<br />' : ''; ?>
-
-					<?php if($footer_menu): ?>
-					<?php
-						wp_nav_menu(
-							array(
-								'menu'           	=> $footer_menu,
-								'container'         => 'div',
-								'container_class'   => 'footer-menu',
-								'menu_class'        => '',
-								'items_wrap'        => '<ul>%3$s</ul>',
-								'depth'             => 1
-							)
-						);
-					?>
-					<?php endif; ?>
-
-				</div>
-				<?php endif; ?>
-
-				<?php if($footer_text_right): ?>
-				<div class="col-md-<?php echo $footer_text || $footer_menu ? 6 : 12; ?>">
-
-					<?php echo do_shortcode($footer_text_right); ?>
-
-				</div>
-				<?php endif; ?>
-			</div>
-
-		</div>
-	</div>
+    </div>
 
 </footer>
