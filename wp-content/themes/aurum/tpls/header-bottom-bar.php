0450b56299ba46253a09b54e9bcf8b18ee3432f4
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-01-07 17:28:24 +0100

    add custom header link in a single file

--- wp-content/themes/aurum/tpls/header-bottom-bar.php
+++ wp-content/themes/aurum/tpls/header-bottom-bar.php
@@ -1,1 +1,10 @@
-AAAAA
+<div class="site-bottom-bar">
+    <div class="container">
+        <div class="row">
+            <?php 
+            @include('link-header_custom.php')
+            ?>
+        </div>
+    </div>
+</div>
+<!-- /.site-bottom-bar -->
--- wp-content/themes/aurum/tpls/header.php
+++ wp-content/themes/aurum/tpls/header.php
@@ -25,25 +25,10 @@ $has_secondary_menu = $header_type && has_nav_menu('secondary-menu');
 			</div>
 			<div class="col-md-6">
 				<div class="menu-right">
-					<div class="button-custom-acquista-biglietti button-only-text button-only-text_black">
-						<a href="<?php echo the_field('header_button_acquista_i_biglietti', 'options'); ?>">
-							<svg width="36" height="12" viewBox="0 0 36 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M24.6667 6C24.6667 8.94552 27.0545 11.3333 30 11.3333C32.9455 11.3333 35.3333 8.94552 35.3333 6C35.3333 3.05448 32.9455 0.666669 30 0.666669C27.0545 0.666669 24.6667 3.05448 24.6667 6ZM-8.74228e-08 7L30 7L30 5L8.74228e-08 5L-8.74228e-08 7Z" fill="#2B2B2B" />
-							</svg>
-							<div class="text">
-								Acquista i biglietti
-							</div>
-						</a>
-					</div>
-					<div class="button-custom-acquista-biglietti button-only-text button-only-text_light button-only-text_magenta">
-						<svg width="36" height="12" viewBox="0 0 36 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M24.6667 6C24.6667 8.94552 27.0545 11.3333 30 11.3333C32.9455 11.3333 35.3333 8.94552 35.3333 6C35.3333 3.05448 32.9455 0.666669 30 0.666669C27.0545 0.666669 24.6667 3.05448 24.6667 6ZM-8.74228e-08 7L30 7L30 5L8.74228e-08 5L-8.74228e-08 7Z" fill="#2B2B2B" />
-						</svg>
-						<div class="text">
-							Prossimi appuntamenti
-						</div>
-					</div>
-					
+					<?php
+					@include('link-header_custom.php');
+					?>
+
 
 				</div>
 				<div class="active-menu-full-page">
