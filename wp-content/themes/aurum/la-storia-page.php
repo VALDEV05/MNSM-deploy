f1fb18c8eb2db5979cf7d895310a865fac7c5a72
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-01-12 17:22:04 +0100

    started history page

---
+++ wp-content/themes/aurum/la-storia-page.php
@@ -0,0 +1,50 @@
+<section class="container-page-la-storia">
+    <section class="container-page-la-storia_title">
+        <div class="container">
+            <h4>La nostra <span>Storia</span></h4>
+        </div>
+        <div class="element-decoration-title-la-storia">
+            <img src="../../../wp-content/uploads/2024/01/background-sheet-music.svg" alt="">
+        </div>
+    </section>
+    <!-- /.container-page-la-storia_title -->
+    <section class="container-page-la-storia_text">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <!-- RENDERE DINAMICO -->
+                    <h2>La storia del museo e delle collezioni</h2>
+                    <p>
+                        Il Museo Nazionale degli Strumenti Musicali di Roma, costituito in gran parte dalla vasta ed eterogenea collezione privata <strong>di Evan Gorga, accoglie oggi circa <span>tremila esemplari di varia epoca e provenienza</span></strong>, dei quali oltre ottocento sono esposti.
+                    </p>
+                    <!-- /RENDERE DINAMICO -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.container-page-la-storia_text -->
+    <section class="container-page-la-storia_container-main-page">
+        <div class="element-background-decoration_storia-main-section">
+            <svg width="1212" height="5607" viewBox="0 0 1212 5607" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1358 1H616.227V410.195H129.047V770.665H773.785V1260.02H1V1764.5H480.177H859.316V2097.8H458.168H69.5252V2700.33H637.735H1003.37V3214.83H69.5252V3753.5H739V4240.5H110V5606.5" stroke="#E2E0DC" stroke-dasharray="14 14" />
+            </svg>
+
+        </div>
+        <div class="container-page-la-storia_container-main-page_container-element">
+            <div class="date-first date">
+                1865
+            </div>
+            <div class="image-first dotted">
+                <img src="../../../wp-content/uploads/2024/01/1985-storia.png">
+            </div>
+            <div class="text-first text-circle">
+                <p>Evangelista Gorga nacque da una famiglia di nobili origini. Grande appassionato di musica, aveva particolari doti canore e intraprese una folgorante carriera come tenore lirico, durata appena quattro anni.
+                    Nel 1989 si ritirò definitivamente dalle scene per dedicarsi al collezionismo.
+                    La sua collezione raggiunse il numero di circa 150000 pezzi, suddivisi in più di trenta sezioni, tra cui strumenti musicali e spartiti, lucerne, affreschi, bilance, giocattoli e oltre cinquemila libri. La passione per il collezionismo lo porterà a dilapidare il suo intero patrimonio e a contrarre ingenti debiti.</p>
+            </div>
+        </div>
+        <!-- /.container-page-la-storia_container-main-page_container-element -->
+    </section>
+    <!-- /.container-page-la-storia_container-main-page -->
+</section>
+<!-- /.container-page-la-storia -->Ì
---
+++ wp-content/themes/aurum/la-storia-template.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Template Name: Storia Museo
+*/
+
+/**
+ *	Aurum WordPress Theme
+ *
+ *	Laborator.co
+ *	www.laborator.co
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Direct access not allowed.
+}
+
+get_header();
+
+@include('la-storia-page.php');
+
+get_footer(); 
