b57f2720e443116caadae62814093a82e012459d
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-01-19 22:48:24 +0100

    add title fixed appointments

--- wp-content/themes/aurum/calendario_museo.php
+++ wp-content/themes/aurum/calendario_museo.php
@@ -59,7 +59,7 @@
                 </div>
                 <!-- /.col-md-8 -->
                 <div class="col-md-3">
-                    
+
                     <?php @include('calendar_element/active_single_event.php') ?>
                 </div>
                 <!-- /.col-md-3 -->
@@ -69,5 +69,34 @@
         <!-- /.container -->
     </section>
     <!-- /.container-page-calendar_main-content -->
+
+    <section class="container-page-calendar_fixed-appoinments">
+        <section class="container-page-calendar_title ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <h4>Appuntamenti <strong>fissi</strong></h4>
+                    </div>
+                    <!-- /.col-md-7 -->
+                    <div class="container-rassegne">
+                        <img src="../../..//wp-content/uploads/2024/01/cta-icon-yellow.svg" alt="">
+                        <div class="container-rassegne_text">
+                            Rassegne & Progetti
+                        </div>
+                        <!-- /.container-rassegne_text -->
+                    </div>
+                     <!-- /.container-rassegne -->
+
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
+            <div class="element-decoration-title-la-storia">
+                <img src="../../../wp-content/uploads/2024/01/background-sheet-music.svg" alt="">
+            </div>
+        </section>
+        <!-- /.container-page-calendar_title -->
+    </section>
+    <!-- /.container-page-calendar_fixed-appoinments -->
 </section>
 <!-- /.container-page-calendar -->
